7828a68197b0a5ffd206a8f81f9a6c251fc56a39
Saharat Saengsawang <saharat.earth@gmail.com>
2017-08-21 17:33:27 +0700

    wemos path

--- main/views.py
+++ main/views.py
@@ -10,16 +10,29 @@ from django.http import JsonResponse
 import json
 
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
-isLock = True
+from django.utils import timezone
+
+from main.models import Lock
+from main.models import Log
 
 def lock(request,mac):
-	isLock = True
-	return HttpResponse(isLock)
+	lock = Lock.objects.get(mac=mac)
+	if lock.isLock == False:
+		lock.isLock = True
+		lock.save()
+		Log.objects.create(lock=lock,user=User.objects.get(username="unknow"),action=3,date=timezone.now())
+	return HttpResponse(lock.isLock)
 
 def unlock(request,mac):
-	isLock = False
-	return HttpResponse(isLock)
+	lock = Lock.objects.get(mac=mac)
+	if lock.isLock == True:
+		lock.isLock = False
+		lock.save()
+		Log.objects.create(lock=lock,user=User.objects.get(username="unknow"),action=4,date=timezone.now())
+	return HttpResponse(lock.isLock)
 
 def state(request,mac):
+	isLock = Lock.objects.get(mac=mac).isLock
 	return HttpResponse(isLock)
